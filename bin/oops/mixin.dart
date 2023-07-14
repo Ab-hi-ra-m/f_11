f12ad90974fa693f64e476ae074854b70dc226df
Ab-hi-ra-m <abhiramabhiram1212@gmail.com>
2023-07-14 14:17:55 +0530

    Initial Commit

---
+++ bin/oops/mixin.dart
@@ -0,0 +1,36 @@
+mixin A
+ {
+  int a =20;
+  int b =60;
+  void sum()
+  {
+    print("Sum = ${a+b}");
+  }
+  void show();//abstract function
+ }
+ mixin B
+ {
+   int c = 30;
+   void display();
+ }
+ class C with A,B
+ {
+  @override
+  void display()
+  {
+    print("Sum=${a+b+c}");
+  }
+
+  @override
+  void show()
+  {
+    print("Sub = ${a-b}");
+  }
+ }
+ void main()
+ {
+   C o = C();
+   o.sum();
+   o.show();
+   o.display();
+ }
